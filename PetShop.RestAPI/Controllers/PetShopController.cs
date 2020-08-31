c4d1bc98c2ff6203c4076da87beb0fb864fcfb52
dreamy <31621059+dreamyy1@users.noreply.github.com>
2020-08-31 17:03:13 +0200

    nothing works

---
+++ PetShop.RestAPI/Controllers/PetShopController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PetShop.Core.ApplicationServices;
+using PetShop.Core.Entity;
+
+namespace PetShop.RestAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PetShopController : ControllerBase
+    {
+        private readonly IPetService _petService;
+
+        public PetShopController(IPetService petService)
+        {
+            _petService = petService;
+        }
+
+
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<PetShopController> _logger;
+
+        public PetShopController(ILogger<PetShopController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Pet>> Get()
+        {
+            return _petService.GetPets();
+        }
+    }
+}
